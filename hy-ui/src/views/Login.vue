b20c049455a640d4684195c72f1c81c330657773
xuanwang <xuanwang@ncs.com.sg>
2018-12-27 13:17:57 +0800

    工具模块封装

--- hy-ui/src/views/Login.vue
+++ hy-ui/src/views/Login.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="page">
     <h2>Login Page</h2>
+    <Button type="primary" @click="login()">登陆</Button>
   </div>
 </template>
 
 <script>
+import mock from '@/mock/index.js';
+import Cookies from 'js-cookie';
+import router from '@/router';
 export default {
-  name: 'Login'
+  name: 'Login',
+  methods: {
+    login(){
+      this.$api.login().then(function(res){
+        alert(res.data.token)
+        Cookies.set('token',res.data.token)
+        router.push('/')
+      }).catch(function(res){
+        alert(res);
+      });
+    } 
+  }
 }
 </script>
